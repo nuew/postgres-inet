b49a729ba3799d9c3d85ebb557d4c67bfbd67aa5
Nuew <code@nuew.net>
2017-04-23 16:42:14 -0400

    Fix versioning on documentation links.

--- src/lib.rs
+++ src/lib.rs
@@ -21,7 +21,7 @@
 //!
 //! [1]: https://crates.io/crates/postgres
 //! [2]: https://github.com/sfackler
-#![doc(html_root_url="https://docs.rs/postgres-inet/0.1.0")]
+#![doc(html_root_url="https://docs.rs/postgres-inet/0.1.1")]
 #![warn(missing_docs)]
 
 #[macro_use]
