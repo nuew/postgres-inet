9603e2bd9db1f704358623ec0c7212fe69d45cea
Ethan Welker <code@nuew.net>
2018-12-10 16:56:20 -0500

    Bump version to 0.15.2

--- src/lib.rs
+++ src/lib.rs
@@ -21,7 +21,7 @@
 //!
 //! [1]: https://crates.io/crates/postgres
 //! [2]: https://github.com/sfackler
-#![doc(html_root_url = "https://docs.rs/postgres-inet/0.15.1")]
+#![doc(html_root_url = "https://docs.rs/postgres-inet/0.15.2")]
 #![deny(
     missing_copy_implementations,
     missing_debug_implementations,
